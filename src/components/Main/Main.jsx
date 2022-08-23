fc3b841d7079f5a85ba46a526efe0654f6a8eae0
Henry Januário <primeirohenry@gmail.com>
2022-08-23 05:03:34 -0300

    Seção dos produtos finalizada

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,58 +1,69 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import FormIndication from '../FormIndication/FormIndication'
-import P from '../P/P'
-import FirstSection from '../Section/FirstSection'
-import Separator from '../Separator/Separator'
-import S from "../Main/Main.module.css"
+import FormIndication from "../FormIndication/FormIndication";
+import P from "../P/P";
+import FirstSection from "../Section/FirstSection";
+import Separator from "../Separator/Separator";
+import S from "../Main/Main.module.css";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
 const Main = () => {
   const [informacoes, setInformacoes] = useState([]);
-  const [products, setProducts] = useState(1)
+  const [products, setProducts] = useState(1);
 
   async function handleRequisicao() {
     const response = await fetch(
       `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${products}`
     );
     const json = await response.json();
-    setInformacoes([...informacoes,...json.products]);
+    setInformacoes([...informacoes, ...json.products]);
   }
 
   useEffect(() => {
     handleRequisicao();
   }, [products]);
 
-  const nextProducts = ()=>{
-    setProducts(products + 1)
-  }
+  const nextProducts = () => {
+    setProducts(products + 1);
+  };
 
   return (
     <div>
       <FirstSection />
       <Separator texto={"Sua seleção especial"} />
       <div className={S.cards}>
-          {informacoes.length>0 &&
-            informacoes.map((item,index) => {
-              return (
-                <Card
-                  key={index}
-                  img={item.image}
-                  nomeDoProduto={item.name}
-                  descricao={item.description}
-                  valorDe={item.oldPrice}
-                  valorPor={item.price}
-                  valorVezes={item.installments.count}
-                  valorOu={item.installments.value}
-                />
-              );
-            })}
-        </div>
-      <Button onClick={nextProducts} texto={"Ainda mais produtos aqui"} />
+        {informacoes.length > 0 &&
+          informacoes.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                img={item.image}
+                nomeDoProduto={item.name}
+                descricao={item.description}
+                valorDe={item.oldPrice}
+                valorPor={item.price}
+                valorVezes={item.installments.count}
+                valorOu={item.installments.value}
+              />
+            );
+          })}
+      </div>
+      <div className={S.buttonProducts}>
+        <Button
+          onClick={nextProducts}
+          texto={"Ainda mais produtos aqui"}
+          className={S.button}
+        />
+      </div>
       <Separator texto={"Compartilhe a novidade"} />
-      <P texto={"Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!"} className={S.p}/>
+      <P
+        texto={
+          "Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!"
+        }
+        className={S.p}
+      />
       <FormIndication />
     </div>
   );
