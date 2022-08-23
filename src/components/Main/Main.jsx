a6aeb9d9256642b31ab3868bcd11f302046a976d
Henry Januário <primeirohenry@gmail.com>
2022-08-23 04:06:36 -0300

    Requisição funcionando *-*

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import FormIndication from '../FormIndication/FormIndication'
 import P from '../P/P'
 import FirstSection from '../Section/FirstSection'
@@ -7,12 +9,43 @@ import S from "../Main/Main.module.css"
 import Card from "../Card/Card";
 
 const Main = () => {
+  const [informacoes, setInformacoes] = useState([]);
+  const [products, setProducts] = useState(1)
+
+  async function handleRequisicao() {
+    const response = await fetch(
+      `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${products}`
+    );
+    const json = await response.json();
+    setInformacoes([...informacoes,...json.products]);
+  }
+
+  useEffect(() => {
+    handleRequisicao();
+  }, [products]);
+
 
   return (
     <div>
       <FirstSection />
       <Separator texto={"Sua seleção especial"} />
-      <Card/>
+      <div className={S.cards}>
+          {informacoes.length>0 &&
+            informacoes.map((item,index) => {
+              return (
+                <Card
+                  key={index}
+                  img={item.image}
+                  nome={item.name}
+                  desc={item.description}
+                  valorDe={item.oldPrice}
+                  valorPor={item.price}
+                  valorVezes={item.installments.count}
+                  ou={item.installments.value}
+                />
+              );
+            })}
+        </div>
       <Separator texto={"Compartilhe a novidade"} />
       <P texto={"Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!"} className={S.p}/>
       <FormIndication />
